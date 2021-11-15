7bd651aea357728b308b567785ca0ef1301aad8f
John Fredy Triana Camacho <john1988triana@gmail.com>
2021-11-15 14:32:25 -0500

    Variables globales

---
+++ src/utils/config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports.App = {
+    PORT: 7000
+}
+
+module.exports.Db = {
+    HOST: "localhost",
+    PORT: 3318,
+    USER: "root",
+    PASSWORD: "",
+    DB: "tutorial"
+}
